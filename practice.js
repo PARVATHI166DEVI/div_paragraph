32cc0fe84daa7d24b9e608bba88b60ddacbf0793
PARVATHI166DEVI <66837819+PARVATHI166DEVI@users.noreply.github.com>
2021-11-18 19:10:10 +0530

    adding files

---
+++ practice.js
@@ -0,0 +1,25 @@
+function red(){
+    if(document.getElementById('div1').style.backgroundColor=="aqua"){
+        document.getElementById('div1').style.backgroundColor = "red";
+    }
+    else{
+        document.getElementById('div1').style.backgroundColor="aqua"
+    }
+}
+
+function green(){
+    document.getElementById('p1').style.color="green";
+}
+
+
+function blue(){
+    // document.getElementById('p1').style.color = "green";
+    if(document.getElementById('div1').style.backgroundColor="blue"){
+        document.getElementById('div1').style.backgroundImage="url(https://i.pinimg.com/originals/02/e9/36/02e9369be42adc6f154f207c38fa8eb3.jpg)"
+    }
+    else{
+        document.getElementById('div1').style.backgroundColor="blue";
+    }
+}
+
+
